8ae3380c3039ca52bf8ad7d9469992d9811a83b5
Maximilian Faessler <maximilian@faesslerweb.de>
2019-03-29 10:11:29 +0000

    [ADD] Commit for data transfer

--- web/typo3conf/ext/hgon/Configuration/TypoScript/Setup/Config/Content.ts
+++ web/typo3conf/ext/hgon/Configuration/TypoScript/Setup/Config/Content.ts
@@ -2,10 +2,6 @@ tt_content.stdWrap.innerWrap.cObject = CASE
 tt_content.stdWrap.innerWrap.cObject {
     key.field = layout
 
-    # Zitat
-    13 = TEXT
-    13.value = <blockquote class="quote / align-center mt-4 mb-4">|</blockquote>
-
     # Sitemap
     50 = TEXT
     50.value = <ul class="menu menu--list">|</ul>
