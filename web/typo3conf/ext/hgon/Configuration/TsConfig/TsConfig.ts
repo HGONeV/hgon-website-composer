8e87fdf8b075efbb1169d694c7ff0f8e1deb6ab0
Maximilian Faessler <maximilian@faesslerweb.de>
2019-04-23 07:28:41 +0000

    [ADD] Some stuff and language

--- web/typo3conf/ext/hgon/Configuration/TsConfig/TsConfig.ts
+++
@@ -1,5 +0,0 @@
-// TsConfig
-<INCLUDE_TYPOSCRIPT: source="DIR:EXT:hgon/Configuration/TsConfig/BackendLayouts" extensions="ts">
-<INCLUDE_TYPOSCRIPT: source="DIR:EXT:hgon/Configuration/TsConfig/GridElements" extensions="ts">
-<INCLUDE_TYPOSCRIPT: source="DIR:EXT:hgon/Configuration/TsConfig/RTE" extensions="ts">
-<INCLUDE_TYPOSCRIPT: source="DIR:EXT:hgon/Configuration/TsConfig/TceForm" extensions="ts">
